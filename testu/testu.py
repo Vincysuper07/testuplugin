47192ced0c22390848b5432faf777840437afbd9
Vincysuper07 <52707876+Vincysuper07@users.noreply.github.com>
2020-03-23 13:16:30 +0100

    Create testu.py

---
+++ testu/testu.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+
+class testù(commands.Cog):
+      def __init__(self, bot):
+            self.bot = bot
+
+    @tasks.loop(seconds=1)
+    await ctx.send("testù")
+
+def setup(bot):
+      bot.add_cog(ServStatus(bot))
